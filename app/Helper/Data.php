4dc1d67cd214de29dc37963566497f6f92ab9aa8
elliotjere <elliotjere28@gmail.com>
2023-04-29 16:18:44 +0300

    update

--- app/Helper/Data.php
+++ app/Helper/Data.php
@@ -8,19 +8,92 @@ class Data
     [
         //question start
         [
-            'question'=>'hgahshgshaghadghaghasgahgs',
+            'question'=>'What is the largest organ in the human body',
             'answer'=>[
                 [
-                    'value'=>'shdhsdgshdshdgshdg',
+                    'value'=>'skin',
                     'is_correct'=>true
                 ],
                 [
-                    'value'=>'shdhsdgshdshdgshdg',
+                    'value'=>'Heart',
                     'is_correct'=>false
                 ]
 
             ]
         ],
+                [
+            'question'=>'Which planet in our solar system is known for its rings',
+            'answer'=>[
+                [
+                    'value'=>'Saturn',
+                    'is_correct'=>true
+                ],
+                [
+                    'value'=>'Mars',
+                    'is_correct'=>false
+                ]
+
+            ]
+        ],
+        [
+            'question'=>'What is the process by which plants convert sunlight into energy called?',
+            'answer'=>[
+                [
+                    'value'=>'Photosynthesis',
+                    'is_correct'=>true
+                ],
+                [
+                    'value'=>'Respiration',
+                    'is_correct'=>false
+                ]
+
+            ]
+        ],
+        [
+            'question'=>'What is the name of the smallest bone in the human body?',
+            'answer'=>[
+                [
+                    'value'=>'Stapes',
+                    'is_correct'=>true
+                ],
+                [
+                    'value'=>'Humerus',
+                    'is_correct'=>false
+                ]
+
+            ]
+        ],
+        [
+            'question'=>'Which war was fought between the United States and Spain in 1898?',
+            'answer'=>[
+                [
+                    'value'=>'Spanish-American War',
+                    'is_correct'=>true
+                ],
+                [
+                    'value'=>'World War I',
+                    'is_correct'=>false
+                ]
+
+            ]
+        ],
+        [
+            'question'=>'Who is the riches man in the world',
+            'answer'=>[
+                [
+                    'value'=>'Elon Musk',
+                    'is_correct'=>true
+                ],
+                [
+                    'value'=>'George Bush',
+                    'is_correct'=>false
+                ]
+
+            ]
+        ],
+
+
+
         //question end
     ];
 }
